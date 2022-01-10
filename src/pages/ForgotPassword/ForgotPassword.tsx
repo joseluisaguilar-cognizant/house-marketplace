0de85fb8eef54c38a50ba4ad7156cadbfc2ac10b
joseluisaguilar-cognizant <JoseLuis.AguilarGomez@cognizant.com>
2022-01-10 15:56:11 +0100

    Reset password

--- src/pages/ForgotPassword/ForgotPassword.tsx
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,7 +1,64 @@
-import { FunctionComponent } from 'react';
+import { ChangeEvent, FormEvent, FunctionComponent, useState } from 'react';
+
+import { Link } from 'react-router-dom';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+
+import { toast } from 'react-toastify';
+
+import { ReactComponent as ArrowRightIcon } from '../../assets/svg/keyboardArrowRightIcon.svg';
 
 const ForgotPassword: FunctionComponent = () => {
-  return <div>Forgot password</div>;
+  const [email, setEmail] = useState<string>('');
+
+  const handleChange = ({
+    target: { value },
+  }: ChangeEvent<HTMLInputElement>) => {
+    setEmail(value);
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success('Email was sent');
+    } catch (error) {
+      toast.error('Could not send reset email');
+    }
+  };
+
+  return (
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Forgot password</p>
+      </header>
+
+      <main>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="email"
+            className="emailInput"
+            value={email}
+            onChange={handleChange}
+          />
+          <Link className="forgotPasswordLink" to="/sign-in">
+            Sign In
+          </Link>
+
+          <div className="signInBar">
+            <div className="signInText">Send Reset Link</div>
+            <button className="signInButton">
+              <ArrowRightIcon fill="#fff" width="34px" height="34px" />
+            </button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 };
 
 export default ForgotPassword;
