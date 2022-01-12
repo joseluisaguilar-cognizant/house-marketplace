4b4c56b26b0b71da5cf8f0df126a10c39eed9a85
joseluisaguilar-cognizant <JoseLuis.AguilarGomez@cognizant.com>
2022-01-12 12:11:21 +0100

    Private listing

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,14 +1,24 @@
-import { ChangeEvent, FunctionComponent, useState } from 'react';
+import { ChangeEvent, FunctionComponent, useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 
 import { getAuth, updateProfile } from 'firebase/auth';
-import { updateDoc, doc } from 'firebase/firestore';
+import {
+  updateDoc,
+  doc,
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  deleteDoc,
+} from 'firebase/firestore';
 import { db } from '../../firebase.config';
 
 import { toast } from 'react-toastify';
 
 import arrowRight from '../../assets/svg/keyboardArrowRightIcon.svg';
 import homeIcon from '../../assets/svg/homeIcon.svg';
+import ListingItem from '../../components/ListingItem/ListingItem';
 
 interface IFormData {
   name: string | undefined;
@@ -18,6 +28,9 @@ interface IFormData {
 const Profile: FunctionComponent = () => {
   const auth = getAuth();
 
+  const [loading, setLoading] = useState(true);
+  const [listings, setListings] = useState<any>();
+
   const [changeDetails, setChangeDetails] = useState<boolean>(false);
   const [{ name }, setFormData] = useState<IFormData>({
     name: auth.currentUser?.displayName as string | undefined,
@@ -25,6 +38,33 @@ const Profile: FunctionComponent = () => {
   });
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUserListings = async () => {
+      const listingsRef = collection(db, 'listings');
+
+      const q = query(
+        listingsRef,
+        where('userRef', '==', auth.currentUser!.uid)
+      );
+
+      const querySnap = await getDocs(q);
+
+      let listings: Array<any> = [];
+
+      querySnap.forEach((doc: any) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings(listings);
+      setLoading(false);
+    };
+
+    fetchUserListings();
+  }, [auth.currentUser!.uid]);
 
   const logout = () => {
     auth.signOut();
@@ -52,6 +92,20 @@ const Profile: FunctionComponent = () => {
       }
     } catch (error) {
       toast.error('Could not update profile details');
+    }
+  };
+
+  const deleteItem = async (id: any) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      await deleteDoc(doc(db, 'listings', id));
+
+      const updatedListings = listings.filter(
+        (listing: any) => listing.id !== id
+      );
+
+      setListings(updatedListings);
+
+      toast.success('Successfully deleted listing');
     }
   };
 
@@ -97,6 +151,24 @@ const Profile: FunctionComponent = () => {
           <p>Sell or rent your home</p>
           <img src={arrowRight} alt="arrow right" />
         </Link>
+
+        {!loading && listings?.length > 0 && (
+          <>
+            <p className="listingText">Your listings</p>
+            <ul className="listingsList">
+              {listings.map((listing: any) => {
+                return (
+                  <ListingItem
+                    key={listing.id}
+                    listing={listing.data}
+                    id={listing.id}
+                    onDelete={() => deleteItem(listing.id)}
+                  />
+                );
+              })}
+            </ul>
+          </>
+        )}
       </main>
     </div>
   );
